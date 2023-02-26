225abfaccd5b483974c070f5c687198be8e27a6e
majoloso97 <majoloso97@gmail.com>
2023-02-26 23:44:02 +0100

    :wrench: Change config to manage settings in .json instead of .ini

--- src/core/config.py
+++ src/core/config.py
@@ -1,49 +1,45 @@
 import os
-import configparser
+import json
 
 CONFIG_PATH = os.path.expanduser('~/.etl/')
-CONFIG_FILE = 'config.ini'
+CONFIG_FILE = 'settings.json'
 
 
-def write_settings(config):
+def write_settings(settings):
     with open(f'{CONFIG_PATH}{CONFIG_FILE}', 'w') as configfile:
-        config.write(configfile)
+        configfile.write(settings)
 
 
 def get_global_settings():
     try:
-        # Read config from .ini file
-        config = configparser.ConfigParser()
-        config.read(f'{CONFIG_PATH}{CONFIG_FILE}')
-        if len(config.sections()) == 0:
+        # Read settings from .json file
+        settings = json.loads(f'{CONFIG_PATH}{CONFIG_FILE}')
+        if len(settings.keys()) == 0:
             raise RuntimeError('No available settings')
 
     except Exception:
         # Create .ini file with default values if it doesn't exist
-        config = configparser.ConfigParser()
-        config['DEFAULT']['LOG_TYPE'] = 'C'
-        config['DEFAULT']['DB_AUTH'] = 'P'
-        config['DEFAULT']['DB_HOST'] = '127.0.0.1'
-        config['DEFAULT']['DB_PORT'] = '3306'
-        config['DEFAULT']['DB_USER'] = 'user'
-        config['DEFAULT']['DB_PASS'] = 'root'
-        config['DEFAULT']['DB_NAME'] = 'etl_db'
+        settings = {}
+        settings['LOG_TYPE'] = 'C'
+        settings['DB_AUTH'] = 'P'
+        settings['DB_HOST'] = '127.0.0.1'
+        settings['DB_PORT'] = '3306'
+        settings['DB_USER'] = 'user'
+        settings['DB_PASS'] = 'root'
+        settings['DB_NAME'] = 'etl_db'
 
         if not os.path.exists(CONFIG_PATH):
             os.makedirs(CONFIG_PATH)
-        write_settings(config)
+        write_settings(json.dumps(settings))
 
-    return config['DEFAULT']
+    return settings
 
 
 def set_global_settings(**kwargs):
     settings = get_global_settings()
-    config = configparser.ConfigParser()
 
     for key in kwargs.keys():
-        if key in settings.keys():
-            config['DEFAULT'][key] = kwargs[key]
+         settings[key ]= kwargs[key]
 
-        config['DEFAULT'][key] = settings[key]
-
-    write_settings(config)
+    write_settings(json.dumps(settings))
+    return settings
