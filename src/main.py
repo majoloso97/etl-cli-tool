2c73c8a3281e95c710ebd6a5d6f337523ba06464
majoloso97 <majoloso97@gmail.com>
2023-02-27 14:40:43 +0100

    :memo: Add comments to RUN method

--- src/main.py
+++ src/main.py
@@ -4,11 +4,17 @@ from .etl.extractors.factory import extractor_factory
 
 log = setup_logger('root')
 
-
+# Define RUN command
 @click.command()
-@click.option('-o', '--origin', 'origin', default='csv')
+@click.option('-o', '--origin', 'origin', default='csv',
+              help='Set data source type. Only CSV is currently available.')
 @click.argument("filepath", type=click.Path(exists=True))
 def run(origin, filepath):
+    '''
+    Run ETL pipeline on the specified FILEPATH.
+
+    FILEPATH = Path to the file to be used to pull data from. Must match --origin type.
+    '''
     try:
         loader = extractor_factory(origin)
         loader.import_data(filepath)
