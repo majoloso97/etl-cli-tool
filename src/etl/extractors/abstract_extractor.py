07f013bf7f8ffeef1317e9a4e9940c3a79227083
majoloso97 <majoloso97@gmail.com>
2023-03-03 18:49:33 +0100

    :coffin: Replace columns in Extractor with columns from actual dataset

--- src/etl/extractors/abstract_extractor.py
+++ src/etl/extractors/abstract_extractor.py
@@ -5,13 +5,11 @@ log = logging.getLogger('root')
 
 
 class AbstractExtractor(ABC):
-    COLUMNS = {'symboling', 'normalized-losses', 'make', 'fuel-type',
-               'aspiration', 'num-of-doors', 'body-style', 'drive-wheels',
-               'engine-location', 'wheel-base', 'length', 'width',
-               'height', 'curb-weight', 'engine-type', 'num-of-cylinders',
-               'engine-size', 'fuel-system', 'bore', 'stroke',
-               'compression-ratio', 'horsepower', 'peak-rpm', 'city-mpg',
-               'highway-mpg', 'price'}
+    COLUMNS = {'Row ID', 'Order ID', 'Order Date', 'Ship Date',
+               'Ship Mode', 'Customer ID', 'Customer Name', 'Segment',
+               'Country', 'City', 'State', 'Postal Code', 'Region',
+               'Product ID', 'Category', 'Sub-Category', 'Product Name',
+               'Sales'}
 
     MESSAGES = {
         'extracting': 'Extracting data from {}',
