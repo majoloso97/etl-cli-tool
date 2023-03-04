86e98afe3e43d1c75b00f07957b6cdbeefe5a529
majoloso97 <majoloso97@gmail.com>
2023-03-04 20:03:59 +0100

    :construction: Add DB model to store ETL output records

--- src/core/models.py
+++ src/core/models.py
@@ -12,3 +12,25 @@ class Log(Base):
     feature = Column(String(15))
     level = Column(String(10))
     message = Column(String(150))
+
+
+class SalesRecord(Base):
+    __tablename__ = "sales_records"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_id = Column(String(15))
+    order_date = Column(DateTime())
+    ship_date = Column(DateTime())
+    ship_mode = Column(String(150))
+    customer_id = Column(String(10))
+    customer_name = Column(String(150))
+    segment = Column(String(150))
+    country = Column(String(150))
+    city = Column(String(150))
+    state = Column(String(150))
+    postal_code = Column(String(150))
+    region = Column(String(150))
+    product_id = Column(String(150))
+    category = Column(String(150))
+    sub_category = Column(String(150))
+    product_name = Column(String(150))
+    sales = Column(String(150))
