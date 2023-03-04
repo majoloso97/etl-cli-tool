89761302857232dba350572382b0d4ea13d476b1
majoloso97 <majoloso97@gmail.com>
2023-03-04 20:01:35 +0100

    :sparkles: Add Loader class

---
+++ src/etl/loader/loader.py
@@ -0,0 +1,45 @@
+from src.core.db import DbService
+from src.core.models import SalesRecord
+import logging
+
+log = logging.getLogger('root')
+
+
+class Loader:
+    COLUMNS = {'order_id', 'order_date', 'ship_date', 'ship_mode',
+               'customer_id', 'customer_name', 'segment', 'country',
+               'city', 'state', 'postal_code', 'region', 'product_id',
+               'category', 'sub_category', 'product_name', 'sales'}
+
+    MESSAGES = {
+        'db_connection': 'Starting connection to Database',
+        'saving': 'Bulk saving data',
+        'complete': 'Save transaction is completed',
+        'error': "Data couldn't be loaded. An error occurred during the transaction: {}"
+    }
+
+    def __init__(self, data):
+        self.data = data
+
+    def validate_data(self):
+        pass
+
+    def load_to_db(self):
+        self.build_log(log.info, 'db_connection')
+        db = DbService()
+        try:
+            self.build_log(log.info, 'saving')
+            db.bulk_save(records=self.data.to_dict(orient='records'),
+                            cls=SalesRecord)
+            self.build_log(log.info, 'complete')
+        except Exception as e:
+            self.build_log(log.error, 'error', e)
+
+    def load_data(self):
+        self.validate_data()
+        self.load_to_db()
+
+    def build_log(self, logger, message_key, *args):
+        template = self.MESSAGES[message_key]
+        message = template.format(*args)
+        logger(message, extra={'feature': 'LOAD'})
