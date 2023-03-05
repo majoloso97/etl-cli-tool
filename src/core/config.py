d7435a0afa6ec01d7874e29f5201878f2a7950a1
majoloso97 <majoloso97@gmail.com>
2023-03-05 21:42:33 +0100

    :wrench: Add functions to get settings from env or prompt

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,6 @@
 import os
 import json
+import getpass
 
 CONFIG_PATH = os.path.expanduser('~/.etl/')
 CONFIG_FILE = 'settings.json'
@@ -48,3 +49,37 @@ def set_global_settings(**kwargs):
 
     write_settings(json.dumps(settings))
     return settings
+
+
+def read_db_settings_from_env():
+    db_host = os.getenv(['DB_HOST'], '127.0.0.1')
+    db_port = os.getenv(['DB_PORT'], '3306')
+    db_user = os.getenv(['DB_USER'], 'user')
+    db_pass = os.getenv(['DB_PASS'], 'admin')
+    db_name = os.getenv(['DB_NAME'], 'etl_db')
+    set_global_settings(DB_HOST=db_host,
+                        DB_PORT=db_port,
+                        DB_USER=db_user,
+                        DB_PASS=db_pass,
+                        DB_NAME=db_name)
+
+
+def read_db_settings_from_input():
+    curr = get_global_settings()
+    host = input(f'Database Host (or Enter to use {curr["DB_HOST"]}): ')
+    port = input(f'Database Port (or Enter to use {curr["DB_PORT"]}): ')
+    user = input(f'Database Username (or Enter to use {curr["DB_USER"]}): ')
+    passwrd = getpass.getpass(f'Database Password (or Enter to use existing): ')
+    name = input(f'Database Name (or Enter to use {curr["DB_NAME"]}): ')
+
+    db_host = host if host else curr['DB_HOST']
+    db_port = port if port else curr['DB_PORT']
+    db_user = user if user else curr['DB_USER']
+    db_pass = passwrd if passwrd else curr['DB_PASS']
+    db_name = name if name else curr['DB_NAME']
+
+    set_global_settings(DB_HOST=db_host,
+                        DB_PORT=db_port,
+                        DB_USER=db_user,
+                        DB_PASS=db_pass,
+                        DB_NAME=db_name)
